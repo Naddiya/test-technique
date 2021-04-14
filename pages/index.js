3c78de1e572d0206b635f54e4fec9741be682d2a
Naddiya <nadiamedkouri@gmail.com>
2021-04-14 12:21:27 +0200

    refactor: remove extra-code

--- pages/index.js
+++ pages/index.js
@@ -9,35 +9,3 @@ export default function Home() {
     </ApolloProvider>
   )
 }
-
-
-// export async function getStaticProps( {AnilistClient}) {
-//   console.log(AnilistClient)
-//   // const client = new AnilistClient ;
-
-
-//   const result = await client.query({
-//     query: gql`
-//       query  {
-//         Page (page:1, perPage: 12){
-//           media(source:MANGA, format:MOVIE) {
-//             id
-//             coverImage {
-//               large
-//             }
-//             title {
-//               romaji
-//               english
-//             }
-//           }
-//         }
-//       }
-//     `
-//   })
-
-//   return {
-//     props: {
-//       animes: result.data.Page.media
-//     },
-//   };
-// };
